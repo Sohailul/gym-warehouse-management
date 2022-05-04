f825a48d31885916aaa40de9f2b64cb2410dff93
sohailul <sohailulalam@gmail.com>
2022-05-04 22:28:39 +0600

    login and register page designed

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='row container'>
+      <div className='col-md-6 col-sm-6 mx-auto mt-5 border p-5'>
+        <h3 className='text-success text-center'>Please Login!</h3>
+        <form>
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input type="email" className="form-control" name="email" placeholder="Enter your email address" required />
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" name="password" placeholder="Enter Password" required />
+          </div>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className="mb-3 form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+            </div>
+            <div className="mb-3 form-check">
+              <p className='fw-bold text-center mt-2'>Forgot Password? <span><Link to='/login' className='text-decoration-none'>Reset Password</Link></span></p>
+            </div>
+          </div>
+          <button type="submit" className="btn w-100 d-block" style={{ backgroundColor: "#4B4C78", color: "#fff" }}>Login</button>
+        </form>
+        <div className='mt-5 text-center'>
+          <p className='fw-bold mt-2'>Don’t have an account? <span><Link to='/register' className='text-decoration-none'>Register now</Link></span></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+  const [agree, setAgree] = useState(false);
+  
   return (
-    <div>Register</div>
+    <div className='row container'>
+      <div className='col-md-6 col-sm-6 mx-auto mt-5 border p-5'>
+        <h3 className='text-success text-center'>Please Register!</h3>
+        <form>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input type="text" className="form-control" name="name" placeholder="Enter your name" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input type="email" className="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter your email address" required />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input type="password" className="form-control" name="password" placeholder="Enter Password" required />
+          </div>
+            <div className="mb-3 form-check">
+              <input onClick={() => setAgree(!agree)} type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className={agree ? 'text-success' : 'text-danger'} for="exampleCheck1">I agree to terms and conditions</label>
+            </div>
+          <button disabled={!agree} type="submit" className="btn w-100 d-block" style={{ backgroundColor: "#4B4C78", color: "#fff" }}>Register</button>
+        </form>
+        <div className='mt-5 text-center'>
+          <p className='fw-bold mt-2'>Already have an account? <span><Link to='/login' className='text-decoration-none'>Login</Link></span></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
