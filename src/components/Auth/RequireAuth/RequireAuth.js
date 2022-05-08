c1b0662beb27a7174d30f4b7b28c2f528d0b497a
sohailul <sohailulalam@gmail.com>
2022-05-08 13:18:07 +0600

    Updated on new use access without email verification

--- src/components/Auth/RequireAuth/RequireAuth.js
+++ src/components/Auth/RequireAuth/RequireAuth.js
@@ -1,16 +1,35 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 
 
 const RequireAuth = ({ children }) => {
     const [user] = useAuthState(auth);
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const navigate = useNavigate();
     let from = location.state?.from?.pathname || "/login";
 
     if (!user) {
         return navigate(from, { replace: true });
+    }
+
+    if (user.providerData[0]?.providerId ==='password' && !user.emailVerified) {
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your Email is not verified!!</h3>
+            <h5 className='text-success'> Please Verify your email address</h5>
+            <button
+            className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                Send Verification Email Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
 
     return children;
